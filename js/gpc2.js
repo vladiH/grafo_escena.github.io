f1024b9a58e49286caa9055c4f1455e1090ead0f
vladimir <yurihuallpavargas@gmail.com>
2022-10-04 12:29:57 +0200

    brazo robotico normales corregido

--- js/gpc2.js
+++ js/gpc2.js
@@ -1,5 +1,6 @@
 import * as THREE from "../lib/three.module.js";
 import * as OrbitControls from '../lib/OrbitControls.module.js'; 
+import {BrazoRobotico} from "./brazo_robotico.js";
 // Variables estandar
 let renderer, scene, camera, orbit;
 
@@ -47,19 +48,8 @@ function loadScene()
     suelo.position.y = -0.2;
     suelo.position.z= 0;
     scene.add(suelo);
-    robot =  baseRobot();
-    const brazo = brazoRobot();
-    const anteBrazo = anteBrazoRobot();
-    const pinzaIzq = pinza();
-    pinzaIzq.position.z = 10
-    const pinzaDer = pinza();
-    pinzaDer.applyMatrix(new THREE.Matrix4().makeScale(1, 1, -1));
-    pinzaDer.position.z = -10
-    brazo.add(anteBrazo);
-    anteBrazo.add(pinzaIzq);
-    anteBrazo.add(pinzaDer);
-    robot.add(brazo);
-    scene.add( robot );
+    robot =  new BrazoRobotico(true)
+    scene.add( robot.model());
 }
 
 function update()
@@ -74,148 +64,4 @@ function render()
     requestAnimationFrame(render);
     update();
     renderer.render(scene,camera);
-}
-
-function pinza(){
-    const geometry = new THREE.BufferGeometry();
-    // create a simple square shape. We duplicate the top left and bottom right
-    // vertices because each vertex needs to appear once per triangle.
-    var vertices = new Float32Array( [      
-        //Left
-        1.0, 0.7, 0.0,
-        1.0, 0.3, 0.0,
-        0.0, 0.0, 0.0,
-        0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        1.0, 0.7, 0.0,
-
-        //rigth horario
-        1.0, 0.7, 0.5,
-        0.0, 1.0, 1.0,
-        0.0, 0.0, 1.0,
-        0.0, 0.0, 1.0,
-        1.0, 0.3, 0.5,
-        1.0, 0.7, 0.5,
-
-        //top
-        0.0, 1.0, 1.0,
-        0.0, 1.0, 0.0,
-        1.0, 0.7, 0.0,
-        1.0, 0.7, 0.0,
-        1.0, 0.7, 0.5,
-        0.0, 1.0, 1.0,
-        
-        //botton hora
-        0.0, 0.0, 1.0,
-        0.0, 0.0, 0.0,
-        1.0, 0.3, 0.0,
-
-        1.0, 0.3, 0.0,
-        1.0, 0.3, 0.5,
-        0.0, 0.0, 1.0,
-
-        //back
-        0.0, 0.0, 1.0,
-        0.0, 1.0, 1.0,
-        0.0, 1.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 0.0,
-        0.0, 0.0, 1.0,
-
-        //front anti
-        1.0, 0.3, 0.5,
-        1.0, 0.3, 0.0,
-        1.0, 0.7, 0.0,
-        1.0, 0.7, 0.0,
-        1.0, 0.7, 0.5,
-        1.0, 0.3, 0.5,
-        
-    ] );
-    var dim = new Array(36).fill([19, 20, 4]).flat();
-    vertices = vertices.map((value,index)=>{
-        return value*dim[index];
-    });
-
-    const normales = [ // 24 x3
-    1,0,0, 1,0,0, 1,0,0, 1,0,0, 1,0,0, 1,0,0,      // Right
-    -1,0,0, -1,0,0, -1,0,0, -1,0,0, -1,0,0, -1,0,0,  // Left
-    0,1,0, 0,1,0, 0,1,0, 0,1,0, 0,1,0, 0,1,0,     // Top 
-    0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0,  // Bottom
-    0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1, // Back 
-    0,0,1, 0,0,1, 0,0,1, 0,0,1, 0,0,1, 0,0,1,     // Front
-                 ];
-    // itemSize = 3 because there are 3 values (components) per vertex
-    geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-    geometry.setAttribute( 'normal', new THREE.Float32BufferAttribute(normales,3));
-    const pinzaMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000 ,wireframe:true} );
-    const mesh = new THREE.Mesh( geometry, pinzaMaterial );
-    mesh.position.x = 9.5;
-    mesh.position.y = -10;
-    mesh.position.z = -2;
-    const boxGeometry = new THREE.Mesh( new THREE.BoxGeometry(19,20,4), pinzaMaterial);
-    boxGeometry.add(mesh);
-    // boxGeometry.rotation.y =90*Math.PI/180;
-    // boxGeometry.position.y =20;
-    //boxGeometry.add(new THREE.AxesHelper(60))
-    boxGeometry.position.y = -80
-    boxGeometry.position.x = 10
-    //boxGeometry.rotation.x =90*Math.PI/180;
-    return boxGeometry;
-}
-
-
-function baseRobot(){
-    const baseMaterial = new THREE.MeshBasicMaterial({color:'red',wireframe:true});
-    return (new THREE.Mesh( new THREE.CylinderGeometry(50,50,15,100), baseMaterial ));
-}
-
-function brazoRobot(){
-    const baseMaterial = new THREE.MeshBasicMaterial({color:'red',wireframe:true});
-    const cylinder = new THREE.Mesh( new THREE.CylinderGeometry(20,20,18,20), baseMaterial );
-    cylinder.rotation.z = 90*Math.PI/180;
-    const rectangle = new THREE.Mesh( new THREE.BoxGeometry(18,120,18,2), baseMaterial );
-    rectangle.rotation.z = 90*Math.PI/180;
-    rectangle.position.x = 60;
-    cylinder.add(rectangle);
-    const sphere = new THREE.Mesh( new THREE.SphereGeometry(20,20,10), baseMaterial );
-    sphere.position.y = -60;
-    rectangle.add(sphere);
-    return cylinder;
-}
-
-function anteBrazoRobot(){
-    const baseMaterial = new THREE.MeshBasicMaterial({color:'red',wireframe:true});
-    const cylinder = new THREE.Mesh( new THREE.CylinderGeometry(22,22,6,20), baseMaterial );
-    cylinder.rotation.z = 90*Math.PI/180;
-    const rectangle1 = new THREE.Mesh( new THREE.BoxGeometry(4,80,4,2), baseMaterial );
-    const rectangle2 = rectangle1.clone();
-    const rectangle3 = rectangle1.clone();
-    const rectangle4 = rectangle1.clone();
-    //rectangle.rotation.z = Math.PI/180;
-    rectangle1.position.y = -40;
-    rectangle1.position.x = 7;
-    rectangle1.position.z = 10;
-
-    rectangle2.position.y = -40;
-    rectangle2.position.x = -7;
-    rectangle2.position.z = 10;
-
-    rectangle3.position.y = -40;
-    rectangle3.position.x = -7;
-    rectangle3.position.z = -10;
-    //rectangle3.position.x = -10;
-    rectangle4.position.y = -40;
-    rectangle4.position.x = 7;
-    rectangle4.position.z = -10;
-    //rectangle4.position.x = 10;
-    cylinder.add(rectangle1);
-    cylinder.add(rectangle2);
-    cylinder.add(rectangle3);
-    cylinder.add(rectangle4);
-    const cylinder1 = new THREE.Mesh( new THREE.CylinderGeometry(15,15,40,20), baseMaterial );
-    cylinder1.position.y = -80;
-    cylinder1.rotation.x = 90*Math.PI/180;
-    cylinder.add(cylinder1);
-    cylinder.position.x = 120;
-    return cylinder;
 }
